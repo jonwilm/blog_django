ab65e65661cad805fc51972f221cd28365e04b82
Jonwilm <jparedes84@gmail.com>
2019-11-29 01:41:32 -0400

    Models Categoria Autor - Vista Admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ('name', 'state', 'date_create',)
+
+class AutorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ('first_name', 'last_name', 'email', 'state', 'date_create',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Autor, AutorAdmin)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('Nombre de la Categoría', max_length=100, null=False, blank=False)
+    state = models.BooleanField('Categoría Activada/Desactivada', default=True)
+    date_create = models.DateField('Fecha de creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        return self.name
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField('Nombre del Autor', max_length=255, null=False, blank=False)
+    last_name = models.CharField('Apellidos del Autor', max_length=255, null=False, blank=False)
+    email = models.EmailField('Email', null=False, blank=False)
+    facebook = models.URLField('Facebook', null=True, blank=True)
+    twitter = models.URLField('Twitter', null=True, blank=True)
+    instagram = models.URLField('Instagram', null=True, blank=True)
+    web = models.URLField('Web', null=True, blank=True)
+    state = models.BooleanField('Autor Activado/Desactivado', default=True)
+    date_create = models.DateField('Fecha de creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return "{0},{1}".format(self.last_name, self.first_name)
