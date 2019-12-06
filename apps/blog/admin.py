aae285d5b346faea4fe3e858855eecad6946135a
Jonwilm <jparedes84@gmail.com>
2019-12-05 23:47:41 -0400

    Dependencia Botones Import Export

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryResources(resources.ModelResource):
+    class Meta:
+        model = Category
+
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name', 'state', 'date_create',)
+    resources_class = CategoryResources
 
-class AutorAdmin(admin.ModelAdmin):
+class AutorResources(resources.ModelResource):
+    class Meta:
+        model = Autor
+
+class AutorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('first_name', 'last_name', 'email', 'state', 'date_create',)
+    resources_class = AutorResources
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Autor, AutorAdmin)
--- blog_django/settings.py
+++ blog_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.blog',
+    'import_export',
 ]
 
 MIDDLEWARE = [
