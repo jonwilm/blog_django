ad54c0b0d805f20854cc03574f036c7f8166209d
Jonwilm <jparedes84@gmail.com>
2019-12-07 00:25:19 -0400

    Herencia de plantillas

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Generales, Programacion, Videojuegos, Tecnologia, Tutoriales
 
 urlpatterns = [
     path('', Home, name = 'index'),
+    path('generales/', Generales, name = 'generales'),
+    path('programacion/', Programacion, name = 'programacion'),
+    path('videojuegos/', Videojuegos, name = 'videojuegos'),
+    path('tecnologia/', Tecnologia, name = 'tecnologia'),
+    path('tutoriales/', Tutoriales, name = 'tutoriales'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'index.html')
+
+def Generales(request):
+    return render(request, 'generales.html')
+
+def Programacion(request):
+    return render(request, 'programacion.html')
+
+def Videojuegos(request):
+    return render(request, 'videojuegos.html')
+
+def Tecnologia(request):
+    return render(request, 'tecnologia.html')
+
+def Tutoriales(request):
+    return render(request, 'tutoriales.html')
